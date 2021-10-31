6858aa477b928bf6b5cd7263a545a2a2d30451ab
Sangram Jagadale <58004845+Sangram3@users.noreply.github.com>
2021-10-31 21:10:38 +0530

    Update and rename knaksack.py to knapsack.py

--- Knapsack_problem/knapsack.py
+++ Knapsack_problem/knapsack.py
@@ -1,10 +1,3 @@
-# A naive recursive implementation
-# of 0-1 Knapsack Problem
-
-# Returns the maximum value that
-# can be put in a knapsack of
-# capacity W
-
 
 def knapSack(W, wt, val, n):
 
@@ -12,10 +5,6 @@ def knapSack(W, wt, val, n):
 	if n == 0 or W == 0:
 		return 0
 
-	# If weight of the nth item is
-	# more than Knapsack of capacity W,
-	# then this item cannot be included
-	# in the optimal solution
 	if (wt[n-1] > W):
 		return knapSack(W, wt, val, n-1)
 
@@ -28,14 +17,11 @@ def knapSack(W, wt, val, n):
 				W-wt[n-1], wt, val, n-1),
 			knapSack(W, wt, val, n-1))
 
-# end of function knapSack
 
 
-#Driver Code
 val = [60, 100, 120]
 wt = [10, 20, 30]
 W = 50
 n = len(val)
 print knapSack(W, wt, val, n)
 
-# This code is contributed by Nikhil Kumar Singh
